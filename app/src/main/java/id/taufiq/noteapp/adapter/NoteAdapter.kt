08cfb55e59ef23343480a8cc806a7b9206a209e9
M. Taufiq Hidayat <mtaufiqh62@gmail.com>
2021-01-02 20:06:16 +0800

    create layout item note.

--- app/src/main/java/id/taufiq/noteapp/adapter/NoteAdapter.kt
+++ app/src/main/java/id/taufiq/noteapp/adapter/NoteAdapter.kt
@@ -1,7 +1,55 @@
 package id.taufiq.noteapp.adapter
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import id.taufiq.noteapp.databinding.FolderRowItemBinding
+import id.taufiq.noteapp.db.note.Notes
+
 /**
  * Created By Taufiq on 1/2/2021.
  *
  */
 
+
+class NoteAdapter(private val onItemClick: (Notes) -> Unit) :
+    ListAdapter<Notes, NoteAdapter.NoteViewHolder>(NoteDiffCallback()) {
+
+    class NoteViewHolder constructor(private val binding: FolderRowItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Notes, itemClick: (Notes) -> Unit) {
+            binding.tvFolderName.text = item.title
+            binding.rootId.setOnClickListener {
+                itemClick(item)
+            }
+
+        }
+
+
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = FolderRowItemBinding.inflate(layoutInflater, parent, false)
+        return NoteViewHolder(binding)
+    }
+
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        holder.bind(getItem(position), onItemClick)
+    }
+
+    class NoteDiffCallback : DiffUtil.ItemCallback<Notes>() {
+        override fun areItemsTheSame(oldItem: Notes, newItem: Notes): Boolean =
+            oldItem.id == newItem.id
+
+        override fun areContentsTheSame(oldItem: Notes, newItem: Notes): Boolean =
+            oldItem == newItem
+
+    }
+
+
+}
